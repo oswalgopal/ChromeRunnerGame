561b2a38cf737ad84939ed70c274f82beed4d0c8
oswalgopal <gploswal@gmail.com>
2020-06-04 23:46:15 +0530

    added files

---
+++ script.js
@@ -0,0 +1,34 @@
+const runner = document.getElementById("runner");
+const object = document.getElementById("object");
+
+document.addEventListener("keypress", (ev) => {
+	if (ev.keyCode === 32) {
+		jump();	
+	}
+});
+
+
+const jump = () => {
+	if (runner.classList !== "runnerAnimation") {
+		runner.classList.add("runnerAnimation");
+		setTimeout( () => {
+			runner.classList.remove("runnerAnimation");
+		}, 500);
+	}	
+}
+let count = 0;
+const checkLose = setInterval(() => {
+	const runnerValue =  parseInt(window.getComputedStyle(runner).getPropertyValue("top"));
+ 	const objectValue =  parseInt(window.getComputedStyle(object).getPropertyValue("left"));
+	console.log("left", objectValue);
+	console.log("top", runnerValue);
+	if ( objectValue < 20 && runnerValue >=450 ) {
+		alert('you lose :( your score is ' +  count / 2);
+		object.style.animate = "null";
+		object.style.display = "none";
+		location.reload();
+	}
+	if (objectValue < 20) {
+		count++;		
+	}
+}, 100);
